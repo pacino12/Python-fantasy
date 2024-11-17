b0e2c20fee4aecc2a3af80f9a4d8e41f8a629d6c
pacino12 <typaccy@gmail.com>
2024-11-17 06:11:18 +0100

    Done messing with Numbers

---
+++ pythonNumbers.py
@@ -0,0 +1,39 @@
+# There are three numeric types in Python:
+
+""""
+int
+float
+complex
+"""
+
+# Example
+import random
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Type Conversion
+x = 1
+y = 3.8
+z = 1j
+# convert from int to float
+a = float(x)
+# convert from float to int:
+b = int(y)
+
+# convert from int to complex:
+c = complex(x)
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+#   RANDOM NUMBER
+
+print(random.randrange(1, 10))
