a201bb204928f6343ea59321887ddb4410c36f9a
pacino12 <typaccy@gmail.com>
2024-11-17 05:17:40 +0100

    Commiting the changes

--- comments.py
+++ comments.py
@@ -1,2 +1,11 @@
 # this is comment
 print("Hello man")
+#the is another way of commenting by using multi line strings """haha"""
+#lets give it a try
+""""This comment
+is just written in multi line string
+crazy innit"""
+print("Thats")
+#let see this where it goes
+#still checking
+
