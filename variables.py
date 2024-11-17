ecba228fc96d2f4c51d114dac18759b8ca09ad15
pacino12 <typaccy@gmail.com>
2024-11-17 05:19:49 +0100

    Just rolling up with varibles

--- variables.py
+++ variables.py
@@ -2,3 +2,19 @@ x = 5
 y = "Marry"
 print(x)
 print(y)
+#Just like Khaled anather one 
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+#casting
+#If you want to specify the data type of a variable, this can be done with casting.
+x=str(3) # Here x will be "3"
+y=int(3) # y will be 3
+z=float(3) # z will be 3.
+
+print(x)
+print(y)
+print(z)
+print("Why is this git commands not chilling")
+print(y)
