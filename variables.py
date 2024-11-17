f42b450e66a30d496d4cd3b24b557e8b9569d1f3
pacino12 <typaccy@gmail.com>
2024-11-17 05:27:59 +0100

    Case sensitive

--- variables.py
+++ variables.py
@@ -2,19 +2,29 @@ x = 5
 y = "Marry"
 print(x)
 print(y)
-#Just like Khaled anather one 
+# Just like Khaled anather one
 x = 4       # x is of type int
-x = "Sally" # x is now of type str
+x = "Sally"  # x is now of type str
 print(x)
 
-#casting
-#If you want to specify the data type of a variable, this can be done with casting.
-x=str(3) # Here x will be "3"
-y=int(3) # y will be 3
-z=float(3) # z will be 3.
+# casting
+# If you want to specify the data type of a variable, this can be done with casting.
+x = str(3)  # Here x will be "3"
+y = int(3)  # y will be 3
+z = float(3)  # z will be 3.
 
 print(x)
 print(y)
 print(z)
 print("Why is this git commands not chilling")
 print(y)
+# Single or dauble quotes
+x = "John"
+y = "peter"  # they are the same actually
+
+
+# what about case sensitive
+# well python is case sensitive
+a = 4
+A = "Sally"
+# A will not overwrite a
