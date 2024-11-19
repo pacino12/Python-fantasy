2f37a26acb16a31acd55b9507d33b4f6eb180145
pacino12 <typaccy@gmail.com>
2024-11-19 02:18:44 +0100

    copying in python wat an adventure

---
+++ copyMethod.py
@@ -0,0 +1,8 @@
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print("we copied the list", mylist)
+
+# we can also use slice operater
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist[:]
+print(mylist)
