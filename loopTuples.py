32f14e7c5cee29e6bd256713d9fee4586ce0000b
pacino12 <typaccy@gmail.com>
2024-11-19 02:59:14 +0100

    looping in tuple

---
+++ loopTuples.py
@@ -0,0 +1,16 @@
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+    print(x)
+# Loop Through the Index Numbers
+print("##########")
+for i in range(len(thistuple)):
+    print(thistuple[i])
+
+
+# Using while loop
+
+thistuple = ("apple", "banana", "cherry")
+i = 0
+while i < len(thistuple):
+    print(thistuple[i])
+    i = i+1
