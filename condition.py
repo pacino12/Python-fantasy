d43db5f1b26c4556f72868b9c30202b79935b9b9
pacino <typaccy@gmail.com>
2025-01-14 19:13:09 +0100

    A little push at

--- condition.py
+++ condition.py
@@ -6,8 +6,17 @@
 
 # If else
 
-x = 31
-if x < 30:
-    print("The Value of X is less than 30")
+# x = 31
+# if x < 30:
+#     print("The Value of X is less than 30")
+# else:
+#     print("x is greater than 30")
+
+# elif
+x = 40
+if x > 40:
+    print("X is less than 40")
+elif x == 40:
+    print("X is equal to 40 ")
 else:
-    print("x is greater than 30")
+    print("It doesnt match")
