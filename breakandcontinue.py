e9d056ca0e55fbbc0196923302636e9d969a8821
pacino <typaccy@gmail.com>
2025-01-17 17:43:46 +0100

    Quit and continue condition

---
+++ breakandcontinue.py
@@ -0,0 +1,26 @@
+# # Break statment
+# for i in "Devops":
+#     print(i)
+#     if i == "o":
+#         print("I got what i need ")
+#         break
+# print("Out of loops")
+
+# Skip
+# Continiue statment
+
+# for i in "Devops":
+#     if i == "o":
+#         print("I got what i need ")
+#         continue
+#     print(f"Value of i is {i}")
+# print("Out of loops")
+import random
+VACCINES = ["Moderna", "Phizer", "Sputnik v", "Covaxin", "ASTRAZENEKA"]
+Lucky = random.choice(VACCINES)
+print(Lucky)
+
+for vac in VACCINES:
+    if vac == Lucky:
+        print(f"{Lucky} vaccine, passed the Test")
+        break
