5b213f42bcac9e1106352125bf795abab27c54b7
pacino <typaccy@gmail.com>
2025-01-17 16:44:12 +0100

    loop fantasy

---
+++ loops.py
@@ -0,0 +1,10 @@
+# # for loop
+# PLANET = "Earth"
+# for i in PLANET:
+#     print("Value of I is now", i)
+# print("Rest of the codes")
+# tuple
+VACCINES = ("Moderna", "Phizer", "Sputnik v", "Covaxin", "AstraZeneca")
+
+for vac in VACCINES:
+    print(f"{vac} Provide Covid 19 immunization")
