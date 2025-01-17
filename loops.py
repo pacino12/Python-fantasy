ead57b9738da1f6ea9df72b5fb372511481effc9
pacino <typaccy@gmail.com>
2025-01-17 16:59:03 +0100

    While loop

--- loops.py
+++ loops.py
@@ -4,7 +4,24 @@
 #     print("Value of I is now", i)
 # print("Rest of the codes")
 # tuple
-VACCINES = ("Moderna", "Phizer", "Sputnik v", "Covaxin", "AstraZeneca")
+
+"""VACCINES = ("Moderna", "Phizer", "Sputnik v", "Covaxin", "AstraZeneca")
 
 for vac in VACCINES:
     print(f"{vac} Provide Covid 19 immunization")
+"""
+
+# List
+VACCINES = ["Moderna", "Phizer", "Sputnik v", "Covaxin", "AstraZeneca"]
+for vac in VACCINES:
+    print(f"{vac} Provide Covid 19 immunization")
+
+# While loop
+# this work over the condition is true
+
+x = 0  # This is an infinite loop
+while x <= 10:
+    print("The value is greater or equal to ten")
+    print("looping")
+    x += 1
+print("The rest of the codes")
